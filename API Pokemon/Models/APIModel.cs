8dfe7f0f555aabe5de7605d02cfbb95d5afe37c5
PieterQQ <pieter444@wp.pl>
2020-06-22 19:22:07 +0200

    Api sent to HTML

--- API Pokemon/Models/APIModel.cs
+++ API Pokemon/Models/APIModel.cs
@@ -1,25 +1,29 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace API_Pokemon.Models
 {
-    public class APIModel
-    {
-    }
+   
     public class Pokemon
     {
-        public string Name { get; set; }
-        public int id { get; set; }
+        [JsonProperty("name")]
+          public string name { get; set; }
+        [JsonProperty("number")]
+        public string number { get; set; }
+        [JsonProperty("supertype")]
         public string supertype { get; set; }
+        [JsonProperty("imageUrl")]
+        public string imageUrl { get; set; }
+        [JsonProperty("subtype")]
         public string subtype { get; set; }
+        [JsonProperty("rarity")]
         public string rarity { get; set; }
     }
     public class RootObject
     {
-        public string status { get; set; }
-        public int totalResults { get; set; }
-        public List<Pokemon> pokemons { get; set; }
+        public List<Pokemon> cards { get; set; }
     }
 }
