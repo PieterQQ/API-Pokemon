24fd4dd05f768d6838b4777401b4e7622a7359a7
PieterQQ <pieter444@wp.pl>
2020-06-22 18:47:49 +0200

    API Model

--- API Pokemon/Controllers/HomeController.cs
+++ API Pokemon/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using API_Pokemon.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,7 +14,14 @@ namespace API_Pokemon.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            WebClient wc = new WebClient();
+            var url = "https://api.pokemontcg.io/v1/cards";
+
+            var pokemonstring = wc.DownloadString(url);
+            byte[] bytes = Encoding.Default.GetBytes(pokemonstring);
+            pokemonstring = Encoding.UTF8.GetString(bytes);
+            RootObject datalist = JsonConvert.DeserializeObject<RootObject>(pokemonstring);
+            return View(datalist);
         }
     }
 }
---
+++ API Pokemon/Models/APIModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_Pokemon.Models
+{
+    public class APIModel
+    {
+    }
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public int id { get; set; }
+        public string supertype { get; set; }
+        public string subtype { get; set; }
+        public string rarity { get; set; }
+    }
+    public class RootObject
+    {
+        public string status { get; set; }
+        public int totalResults { get; set; }
+        public List<Pokemon> pokemons { get; set; }
+    }
+}
